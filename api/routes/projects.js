4b1f96b4f70f69ec7af0ada70d193bbf8edf6fb4
yanis900 <yanis991@hotmail.com>
2025-10-22 19:36:51 +0100

    update: delete project now takes id

--- api/routes/projects.js
+++ api/routes/projects.js
@@ -5,7 +5,7 @@ const tokenChecker = require("../middleware/tokenChecker");
 const router = express.Router();
 
 router.put("/new", tokenChecker, ProjectsController.createProject);
-router.delete("/delete-one", tokenChecker, ProjectsController.deleteProject);
+router.delete("/delete-one/:id", tokenChecker, ProjectsController.deleteProject);
 router.put("/edit", tokenChecker, ProjectsController.editProject);
 
 module.exports = router;
