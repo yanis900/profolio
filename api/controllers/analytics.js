0a66f0c7bb080a9a38a2f3ebf99dc18df9a94146
yanis900 <yanis991@hotmail.com>
2025-10-24 16:37:58 +0100

    bug: now cannot view own portfolio

--- api/controllers/analytics.js
+++ api/controllers/analytics.js
@@ -33,6 +33,10 @@ async function updateViewCount(req, res) {
     return res.status(404).json({ message: "User not found" });
   }
 
+  if (viewerId.toString() === user._id.toString()) {
+  return res.status(400).json({ message: "You can't view your own profile." });
+}
+
   const alreadyViewed = user.analytics.views.find(
     (view) => view.userId?.toString() === viewerId
   );
