edabe47bfa99c970ebad2e905646500bef90d0ed
yanis900 <yanis991@hotmail.com>
2025-10-24 12:30:16 +0100

    Update model to save views with device type

--- api/models/user.js
+++ api/models/user.js
@@ -7,10 +7,14 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   bio: { type: String },
   jobtitle: { type: String },
-  opentowork:{type: Boolean, default: false},
-  location:{type: String},
+  opentowork: { type: Boolean, default: false },
+  location: { type: String },
   links: { type: Array },
-  image: { type: String, default: "https://profile-image-software-dev-project3.s3.eu-west-2.amazonaws.com/default+profile.png"},
+  image: {
+    type: String,
+    default:
+      "https://profile-image-software-dev-project3.s3.eu-west-2.amazonaws.com/default+profile.png",
+  },
   projects: [
     {
       title: { type: String },
@@ -18,6 +22,19 @@ const UserSchema = new mongoose.Schema({
       links: { type: Array },
     },
   ],
+  analytics: {
+    views: [
+      {
+        userId: { type: mongoose.Schema.Types.Mixed }, // can store ObjectId or String (IP)
+        viewedAt: { type: Date, default: Date.now },
+        device: {
+          type: String,
+          enum: ["Desktop", "Mobile", "Tablet", "Unknown"],
+          default: "Unknown",
+        },
+      },
+    ],
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
